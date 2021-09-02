98cb47bd5f6ceea8edd29c83364668182abadd77
Hidden-Bug-0x1 <>
2021-09-02 15:00:00 -0800

    Changed some colors. Allows children to be toggled into view

--- ui/src/App.js
+++ ui/src/App.js
@@ -7,10 +7,12 @@ import Heading from './components/Heading';
 function App() {
   return (
     <div>
-      <Heading title="Suspicious Email" />
-      <Heading title="Inappropriate Post" />
-      <Heading title="Insecure Account Settings" />
-      <Heading title="Weak Password" />
+      <Heading title="Suspicious Email" id={1}>
+        <h1>All my other data!</h1>
+      </Heading>
+      <Heading title="Inappropriate Post" id={2} />
+      <Heading title="Insecure Account Settings" id={3} />
+      <Heading title="Weak Password" id={4} />
     </div>
   );
 }

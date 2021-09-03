383ee734be42ba964071e8d62e2d6b3a4a02bea7
Hidden-Bug-0x1 <goaliegeo30@gmail.com>
2021-09-02 18:00:19 -0800

    Added the layout items to report a problem

---
+++ ui/src/pages/InappropriatePost.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const InappropriatePost = () => {
+  return (
+    <div className='card bg-secondary'>
+      <h1>Social media post was inappropriate</h1>
+    </div>
+  );
+};
+
+export default InappropriatePost;
---
+++ ui/src/pages/WeakPassword.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const WeakPassword = () => {
+	return (
+		<div className='card bg-secondary'>
+			<h1>Tips to strengthen your password</h1>
+		</div>
+	);
+};
+
+export default WeakPassword;

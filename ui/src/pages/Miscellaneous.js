3d75795a752fac7540370ee9d0ea8668eaf47c10
Hidden-Bug-0x1 <goaliegeo30@gmail.com>
2021-09-05 19:57:39 -0800

    Added content to Miscellaneous

--- ui/src/pages/Miscellaneous.js
+++ ui/src/pages/Miscellaneous.js
@@ -2,8 +2,17 @@ import React from 'react';
 
 const Miscellaneous = () => {
 	return (
-		<div className='card bg-secondary'>
-			<h1>other tips and tricks to keep in mind</h1>
+		<div className='card bg-secondary flex-col'>
+			<h1 className='tab-l mb-1'>Good Practices:</h1>
+			<ul className='tab-l mt-0'>
+				<li>Lock your computer when you leave the room</li>
+				<li>Keep your software up to date</li>
+				<li>Ask for assistance when downloading software</li>
+				<li>
+					Take mental and physical breaks
+					<br /> i.e. move around some and relax the brain
+				</li>
+			</ul>
 		</div>
 	);
 };
